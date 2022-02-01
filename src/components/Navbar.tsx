1e789357ea628501fa73934a057fc2754d2b11d1
Josh Dronfield <josh.dronfield.com>
2022-02-01 11:19:22 +0000

    some changes to navbar and moved components folder

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    NavLink
+  } from "react-router-dom";
+import './Navbar.css'
+
+function Navbar () {
+    return (
+        <Router>    
+            <div className = "navbar_cont">
+                <a className='logo'>JOSH</a>
+                <div className="navbutton_cont">
+                    <NavLink to='/' activeClassName='A-home' className = 'navButton'>Home</NavLink>
+                    <NavLink to='/About' activeClassName='A-about' className = 'navButton'>About</NavLink>
+                    <NavLink to='/Interests' activeClassName='A-insterests' className = 'navButton'>Interests</NavLink>
+                    <NavLink to='/work' activeClassName='A-work' className = 'navButton'>work</NavLink>
+                </div>
+            </div>
+        </Router>
+    )
+}
+
+export default Navbar
+
