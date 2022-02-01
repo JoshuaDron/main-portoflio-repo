f9a9cade5162bcda764011da429d700bda0bce2c
Josh Dronfield <josh.dronfield.com>
2022-02-01 13:58:56 +0000

    added portrait image to home page and title

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-// Wbepack & TypeScript image import
-declare module '*.png' {
-    const value: import('react-native').ImageSourcePropType;
-    export default value;
-}
-declare module '*.svg' {
-    const value: import('react-native').ImageSourcePropType;
-    export default value;
-}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+const portrait = require('../img/portrait.png');
 
 const Home = () => {
     return (
-        <div>
+        <div className="container">
+            <h1 className='hello'>HELLO</h1>
+            <img alt='portrait of Josh' className="portrait" src={String(portrait)} />
         </div>
     )
 }
